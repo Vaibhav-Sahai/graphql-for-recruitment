da635b0c6fa1a7d08766b92354ee9e1213d77636
Vaibhav <vaibhavsahai2003@gmail.com>
2022-01-26 13:50:00 -0600

    Added migration table

--- migrations/2022_01_26_032831_create_applicants_table.py
+++ migrations/2022_01_26_032831_create_applicants_table.py
@@ -9,6 +9,22 @@ class CreateApplicantsTable(Migration):
         """
         with self.schema.create('applicants') as table:
             table.increments('id')
+            table.timestamp('timestamp')
+            table.text('email')
+            table.string('name')
+            table.string('year')
+            table.string('major')
+            table.string('second_major')
+            table.string('minor')
+            table.string('second_minor')
+            table.string('gpa')
+            table.string('website')
+            table.enum('division', ['Software Development', 'Strategy Implementation', 'Quanitative Research', 'External Affairs'])
+            table.text('why_team')
+            table.string('commitment')
+            table.text('value_to_club')
+            table.text('personal_goals')
+            table.string('assessment')
             table.timestamps()
 
     def down(self):
